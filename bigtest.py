4d3705d030bb4b88f6fcd00e23664029d51d0e30
MARIOSTAR2 <alskfl7777@naver.com>
2021-10-26 18:26:28 +0900

    2.경로지정(로드)완료

--- bigtest.py
+++ bigtest.py
@@ -3,6 +3,10 @@ import numpy as np
 import pandas as pd
 import sqlalchemy as db
 
+#2.경로 지정 (로드)
 bream_length = pd.read_csv("bream_length.csv").to_numpy().flatten()
-print(bream_length)
+bream_weight = pd.read_csv("bream_weight.csv").to_numpy().flatten()
+smelt_length = pd.read_csv("smelt_length.csv").to_numpy().flatten()
+smelt_weight = pd.read_csv("smlet_weight.csv").to_numpy().flatten()
+#print(bream_length)
 
